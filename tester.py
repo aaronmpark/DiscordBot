3dbb2cbbfaa2baa9e4ccfead536dedf31211e2f3
Aaron Park <135677875+aaronmpark@users.noreply.github.com>
2024-09-07 01:45:26 -0400

    Delete tester.py

--- tester.py
+++
@@ -1,2 +0,0 @@
-thing = [{'leagueId': '7cdbe035-e0f3-4fa5-9016-40176b35df1b', 'queueType': 'RANKED_FLEX_SR', 'tier': 'EMERALD', 'rank': 'I', 'summonerId': 'R5Vd21lmM6HgaTuMLc5M5ROC1erjiE9xxje4Jc2BVi_bn9A', 'summonerName': 'bobapark', 'leaguePoints': 54, 'wins': 15, 'losses': 11, 'veteran': False, 'inactive': False, 'freshBlood': False, 'hotStreak': False}]
-print(thing[0]['queueType'])
